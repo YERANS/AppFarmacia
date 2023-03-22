b975d9cf0976dfccbcd3749117dc618da40bbf82
Yerson Vargas <yersonvargas18@gmail.com>
2023-03-21 20:18:10 -0500

    Añadiendo metodos modificar, eliminar, editar en clase EmployeesDao

--- src/models/EmployeesDao.java
+++ src/models/EmployeesDao.java
@@ -1,9 +1,13 @@
 package models;
 
+import java.sql.Timestamp;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class EmployeesDao {
@@ -58,5 +62,128 @@ public class EmployeesDao {
             JOptionPane.showMessageDialog(null, "error al obtener al empleado " + e);
         }
         return employee;
+    }
+    
+    // Metodo registrar empleado
+    public boolean registerEmployeeQuery(Employees employee){
+        String query = "INSERT INTO employees(id, full_name, username, address, telephone, email, password, rol, created" 
+                + "updated) VALUES(?,?,?,?,?,?,?,?,?,?)";
+        
+        Timestamp datetime = new Timestamp(new Date().getTime());
+        
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.setInt(1, employee.getId());
+            pst.setString(2, employee.getFull_name());
+            pst.setString(3, employee.getUsername());
+            pst.setString(4, employee.getAddress());
+            pst.setString(5, employee.getTelephone());
+            pst.setString(6, employee.getEmail());
+            pst.setString(7, employee.getPassword());
+            pst.setString(8, employee.getRol());
+            pst.setTimestamp(9, datetime);
+            pst.setTimestamp(10, datetime);
+            pst.execute();
+            return true;
+        
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "error al registrar el empleado " + e);
+            return false;            
+        }
+        
+    }
+     
+    //metodo para listar empleado
+    public List listEmployeesQuery(String value){
+        List<Employees> list_employees = new ArrayList ();
+        String query = "SELECT * FROM employees ORDER BY rol ASC";
+        String query_search_employee = "SELECT * FROM employees WHERE id LIKE '%" + value + "%'";
+        
+        try {
+            conn = cn.getConnection();
+            if(value.equalsIgnoreCase("")){
+                pst = conn.prepareStatement(query);
+                rs =  pst.executeQuery();
+            }else{
+                pst = conn.prepareStatement(query_search_employee);
+                rs =  pst.executeQuery();
+            }
+            
+        while (rs.next()) {                
+                Employees employee = new Employees();
+                employee.setId(rs.getInt("id"));
+                employee.setFull_name(rs.getString("full_name"));
+                employee.setUsername(rs.getString("username"));
+                employee.setAddress(rs.getString("address"));
+                employee.setTelephone(rs.getString("telephone"));
+                employee.setEmail(rs.getString("email"));
+                employee.setRol(rs.getString("rol"));
+                list_employees.add(employee);   
+        }
+        } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e.toString());
+        }
+        return list_employees; 
+               
+    }
+    
+    //modificar empleado
+    public boolean updateEmployeeQuery(Employees employee){
+        String query = "UPDATE employees SET full_name = ?, username = ?, address = ?, telephone = ?, email = ?, rol = ?, updated = ?"
+                + "WHERE id = ?";
+        
+        Timestamp datetime = new Timestamp(new Date().getTime());
+        
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.setString(1, employee.getFull_name());
+            pst.setString(2, employee.getUsername());
+            pst.setString(3, employee.getAddress());
+            pst.setString(4, employee.getTelephone());
+            pst.setString(5, employee.getEmail());
+            pst.setString(6, employee.getRol());
+            pst.setTimestamp(7, datetime);
+            pst.setInt(8, employee.getId());
+            pst.execute();
+            return true;
+        
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "error al modificar los datos del empleado " + e);
+            return false;            
+        }
+        
+    }
+    
+    //eliminar empleado
+    public boolean deleteEmployeeQuery(int id) {
+        String query = "DELETE FROM employees WHERE id = " + id;
+        
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.execute();
+            return true;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "no puede eliminar un empleado que tenga relacion con otra tabla");            
+            return false;
+        }   
+               
+    }
+    
+    //cambiar la contraseña
+    public boolean updateEmployeePassword(Employees employee){
+        String query  = "UPDATE employees SET password = ? WHERE username = '" + username_user + "'";
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.setString(1, employee.getPassword());
+            pst.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "a ocurrido unn error al modificar la contraseña " + e);            
+            return false;
+        }
     }
 }
